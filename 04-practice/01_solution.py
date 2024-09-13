a26bb247b2ad57ef2587ad3ddcb0ff8eefc0b9e7
Swaraj-02 <dashswaraj02@gmail.com>
2024-09-13 20:09:21 +0530

    add practice question files

---
+++ 04-practice/01_solution.py
@@ -0,0 +1,9 @@
+# 1. Write a Python program to calculate the area of a rectangle given its length and width.
+
+rectangle_length = int(input('Enter the Length of Rectangle:- '))
+rectangle_width = int(input('Enter the Width of Rectangle:- '))
+
+rectangle_area = rectangle_length * rectangle_width
+
+print('Area of reactangle:-', rectangle_area)
+
---
+++ 04-practice/02_solution.py
@@ -0,0 +1,6 @@
+#2. Create a program that takes a user's name and age as input and prints a greeting message.
+
+user_name = str(input('Enter your Username:- '))
+age = str(input('Enter your Age:- '))
+
+print(f"Welcome {user_name} to this Ultimate Python Course, according to your {age} you are good to go. Thanks")
